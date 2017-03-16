da90b4a506dac654fea2d43d22a492af59190e76
lishuo <lishuo98@yahoo.com.sg>
2017-03-16 16:17:17 +0800

    Finished validation

--- main/validation.py
+++ main/validation.py
@@ -9,6 +9,7 @@ import numpy as np
 from scipy.misc import imread
 import tensorflow as tf
 import os
+import operator
 
 from ssd import SSD300
 from ssd_utils import BBoxUtility
@@ -36,27 +37,24 @@ bbox_util = BBoxUtility(NUM_CLASSES)
 
 inputs = []
 images = []
-'''
-I added this myself, if wrong delete this part only
-'''
-i = 0
+files = []
 for filename in os.listdir('../data/VOC2007/JPEGImages'):
-    # if filename.endswith('.jpg'):
-    if i <=100:
-        img_path = '../data/VOC2007/JPEGImages/' + filename
-        img = image.load_img(img_path, target_size=(300, 300))
-        img = image.img_to_array(img)
-        images.append(imread(img_path))
-        inputs.append(img.copy())
-        i += 1
-        continue
-    else:
-        continue
+    if filename.endswith('.jpg'):
+        files.append(filename)
+
+for filename in sorted(files):
+    img_path = '../data/VOC2007/JPEGImages/' + filename
+    img = image.load_img(img_path, target_size=(300, 300))
+    img = image.img_to_array(img)
+    images.append(imread(img_path))
+    inputs.append(img.copy())
+
 inputs = preprocess_input(np.array(inputs))
 preds = model.predict(inputs, batch_size=1, verbose=1)
 
 results = bbox_util.detection_out(preds)
 
+label = []
 for i, img in enumerate(images):
     # Parse the outputs.
     det_label = results[i][:, 0]
@@ -66,14 +64,26 @@ for i, img in enumerate(images):
     top_indices = [i for i, conf in enumerate(det_conf) if conf >= 0.6]
 
     top_conf = det_conf[top_indices]
-    top_label_indices = det_label[top_indices].tolist()
+    label.append(det_label[top_indices].tolist())
 
-        #label = a number 1 - 20
-print(top_label_indices)
-input('a')
+print(label)
 
 import pickle
 with open('../data/VOC2007.pkl', 'rb') as read:
-    a = pickle.load(read)
-    for i in a:
-        somethinf
+    x = pickle.load(read)
+    sorted_x = sorted(x.items(), key=operator.itemgetter(0))
+    number_correct = 0
+    total = 0
+    for i, j in enumerate(sorted_x):
+        lists = j[1]
+        total += len(lists)
+        for k, a_list in enumerate(lists):
+            try:
+                if int(a_list[int(label[i][int(k)])]) == 1:
+                    number_correct += 1
+            except:
+                print('missed one')
+                continue
+
+    correct = number_correct/total*100
+    print('Correct: {}\nTotal: {}\nPercentage: {}'.format(number_correct, total, correct))
