728b1ff0363b7045a3d43ffa5a0ba7a9dc761579
lishuo <lishuo98@yahoo.com.sg>
2017-04-03 15:43:46 +0800

    To be furthur worked on

--- main/model/test.py
+++ main/model/test.py
@@ -1,5 +1,15 @@
 import ml_metrics as metrics
 import xml.etree.ElementTree as ET
+import matplotlib.pyplot as plt
+import numpy as np
+import tensorflow as tf
+from keras.preprocessing import image
+from keras.backend.tensorflow_backend import set_session
+from scipy.misc import imread
+from keras.applications.imagenet_utils import preprocess_input
+
+from ssd import SSD300
+from ssd_utils import BBoxUtility
 
 def parse_rec(filename):
     """ Parse a PASCAL VOC xml file """
@@ -16,6 +26,104 @@ def parse_rec(filename):
         objects.append(obj_struct)
 
     return objects
+
+def get_ground_truth(image_file):
+    with open(image_file, 'r') as f:
+        lines = f.readlines()
+    imagenames = [x.strip() for x in lines]
+
+    # Gets ground truth boxes with independent list for each image.
+    ground_truth = []
+    for img in imagenames:
+        file_temp = []
+        filename = '../../data/VOC2007/Annotations/' + img + '.xml'
+        objects = parse_rec(filename)
+        for i in objects:
+            file_temp.append(i['name'])
+            file_temp.append(i['bbox'])
+
+        ground_truth.append(file_temp)
+
+    ground_truth = tuple(ground_truth)
+
+    return imagenames, ground_truth
+
+def load_model():
+    # matplotlib inline
+    plt.rcParams['figure.figsize'] = (8, 8)
+    plt.rcParams['image.interpolation'] = 'nearest'
+
+    np.set_printoptions(suppress=True)
+
+    config = tf.ConfigProto()
+    config.gpu_options.per_process_gpu_memory_fraction = 0.45
+    set_session(tf.Session(config=config))
+
+    voc_classes = ['Aeroplane', 'Bicycle', 'Bird', 'Boat', 'Bottle',
+                   'Bus', 'Car', 'Cat', 'Chair', 'Cow', 'Diningtable',
+                   'Dog', 'Horse','Motorbike', 'Person', 'Pottedplant',
+                   'Sheep', 'Sofa', 'Train', 'Tvmonitor']
+    NUM_CLASSES = len(voc_classes) + 1
+
+    input_shape=(300, 300, 3)
+    model = SSD300(input_shape, num_classes=NUM_CLASSES)
+    model.load_weights('../../data/weights_SSD300.hdf5', by_name=True)
+    bbox_util = BBoxUtility(NUM_CLASSES)
+
+    return model, bbox_util, voc_classes
+
+def validates_images(imagenames, model, bbox_util):
+    inputs = []
+    images = []
+    for filename in imagenames:
+        img_path = '../../data/VOC2007/JPEGImages/' + filename + '.jpg'
+        img = image.load_img(img_path, target_size=(300, 300))
+        img = image.img_to_array(img)
+        images.append(imread(img_path))
+        inputs.append(img.copy())
+
+    inputs = preprocess_input(np.array(inputs))
+    preds = model.predict(inputs, batch_size=1, verbose=1)
+
+    results = bbox_util.detection_out(preds)
+
+    return images, results
+
+def process_images(images, results, voc_classes):
+    image_list = []
+    for i, img in enumerate(images):
+        # Parse the outputs.
+        det_label = results[i][:, 0]
+        det_conf = results[i][:, 1]
+        det_xmin = results[i][:, 2]
+        det_ymin = results[i][:, 3]
+        det_xmax = results[i][:, 4]
+        det_ymax = results[i][:, 5]
+
+        # Get detections with confidence higher than 0.4, as it gives the highest % accuracy of labels.
+        top_indices = [i for i, conf in enumerate(det_conf) if conf >= 0.5]
+
+        top_conf = det_conf[top_indices]
+        top_label_indices = det_label[top_indices].tolist()
+        top_xmin = det_xmin[top_indices]
+        top_ymin = det_ymin[top_indices]
+        top_xmax = det_xmax[top_indices]
+        top_ymax = det_ymax[top_indices]
+
+        a_list = []
+        for i in range(top_conf.shape[0]):
+            xmin = int(round(top_xmin[i] * img.shape[1]))
+            ymin = int(round(top_ymin[i] * img.shape[0]))
+            xmax = int(round(top_xmax[i] * img.shape[1]))
+            ymax = int(round(top_ymax[i] * img.shape[0]))
+            label = int(top_label_indices[i])
+            label_name = voc_classes[label - 1]
+
+            a_list.append([label_name, xmin, ymin, xmax, ymax])
+
+        image_list.append(a_list)
+
+    return predict_label, predict_box
 
 def bb_intersection_over_union(boxA, boxB):
     # determine the (x, y)-coordinates of the intersection rectangle
@@ -40,127 +148,38 @@ def bb_intersection_over_union(boxA, boxB):
     # return the intersection over union value
     return iou
 
-def get_ground_truth(image_file):
-    with open(image_file, 'r') as f:
-        lines = f.readlines()
-    imagenames = [x.strip() for x in lines]
-
-    # Gets name of each image file to validate on
-    images = []
-    for img in imagenames:
-        images.append(img + '.txt')
-
-    # Gets ground truth boxes with independent list for each image.
-    ground_truth = []
-    for img in imagenames:
-        filename = '../../data/VOC2007/Annotations/' + img + '.xml'
-        file_temp = []
-        objects = parse_rec(filename)
-        for i in objects:
-            file_temp.append(i['name'])
-            file_temp.append(i['bbox'])
-        ground_truth.append(file_temp)
-
-    ground_truth = tuple(ground_truth)
-
-    return images, ground_truth
-
-def load_model():
-    # matplotlib inline
-    plt.rcParams['figure.figsize'] = (8, 8)
-    plt.rcParams['image.interpolation'] = 'nearest'
-
-    np.set_printoptions(suppress=True)
-
-    config = tf.ConfigProto()
-    config.gpu_options.per_process_gpu_memory_fraction = 0.45
-    set_session(tf.Session(config=config))
-
-    voc_classes = ['Aeroplane', 'Bicycle', 'Bird', 'Boat', 'Bottle',
-                   'Bus', 'Car', 'Cat', 'Chair', 'Cow', 'Diningtable',
-                   'Dog', 'Horse','Motorbike', 'Person', 'Pottedplant',
-                   'Sheep', 'Sofa', 'Train', 'Tvmonitor']
-    NUM_CLASSES = len(voc_classes) + 1
-
-    input_shape=(300, 300, 3)
-    model = SSD300(input_shape, num_classes=NUM_CLASSES)
-    model.load_weights('../data/weights_SSD300.hdf5', by_name=True)
-    bbox_util = BBoxUtility(NUM_CLASSES)
-
-    return model, bbox_util
-
-def validates_images(images, model, bbox_util):
-    inputs = []
-
-    for filename in images:
-        img_path = '../../data/VOC2007/JPEGImages/' + filename
-        img = image.load_img(img_path, target_size=(300, 300))
-        img = image.img_to_array(img)
-        images.append(imread(img_path))
-        inputs.append(img.copy())
-
-    inputs = preprocess_input(np.array(inputs))
-    preds = model.predict(inputs, batch_size=1, verbose=1)
-
-    results = bbox_util.detection_out(preds)
-
-    return results, img
-
-def process_images(results):
-    image_list = []
-    for i in range(len(results)):
-        a_list = []
-        # Parse the outputs.
-        det_label = results[i][:, 0]
-        det_conf = results[i][:, 1]
-        det_xmin = results[i][:, 2]
-        det_ymin = results[i][:, 3]
-        det_xmax = results[i][:, 4]
-        det_ymax = results[i][:, 5]
-
-        # Get detections with confidence higher than 0.4, as it gives the highest % accuracy of labels.
-        top_indices = [i for i, conf in enumerate(det_conf) if conf >= 0.4]
-
-        # Format of a_list: confidence, labels, xmin, ymin, xmax, ymax
-        a_list.append(det_conf[top_indices])
-        a_list.append(det_label[top_indices].tolist())
-        a_list.append(det_xmin[top_indices])
-        a_list.append(det_ymin[top_indices])
-        a_list.append(det_xmax[top_indices])
-        a_list.append(det_ymax[top_indices])
-
-        image_list.append(a_list)
-
-    return image_list
-
-
 if __name__ == '__main__':
     # Get data
-    images, ground_truth = get_ground_truth('../../data/VOC2007/ImageSets/Layout/val.txt')
+    # imagenames = file name of images to validate on
+    imagenames, ground_truth = get_ground_truth('../../data/VOC2007/ImageSets/Layout/val.txt')
 
     # Formats data
+    # truth = name of object
+    # gt_box = coordinates of bounding box
     truth = []
-    gt = []
+    gt_box = []
     for a in ground_truth:
-        for i, j in enumerate a:
+        temp_truth = []
+        temp_gt_box = []
+        for i, j in enumerate(a):
             if i%2 == 0:
-                truth.append(j)
+                temp_truth.append(j)
             else:
-                gt.append(j)
+                temp_gt_box.append(j)
+        truth.append(temp_truth)
+        gt_box.append(temp_gt_box)
 
     # Load model and performs predictions
-    model, bbox_util = load_model()
-    results, img = validates_images(images, model, bbox_util)
+    model, bboxutil, voc_classes = load_model()
+    images, results= validates_images(imagenames, model, bboxutil)
 
     # Removes unecessary predicitons
-    image_list = process_images(results)
+    predictions = process_images(images, results, voc_classes)
 
+    
+    iou = bb_intersection_over_union(gt_box, predict_box)
 
-
-
-    iou = bb_intersection_over_union(gt, prediction)
-
-    ap = metrics.mapk(truth, prediction, 328) #may need to minus 1
+    ap = metrics.mapk(truth, predict_label, 328) #may need to minus 1
     print('IoU: {}\nAP: {}'.format(iou, ap))
 
 
